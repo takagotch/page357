22cd47c9617c80c2c3c3a868f76fd22762c8b129
takagotch <30462722+takagotch@users.noreply.github.com>
2020-10-07 04:19:08 +0900

    Update nav.js

--- src/nav.js
+++ src/nav.js
@@ -30,7 +30,7 @@ export default {
           icon: 'fa fa-puzzle-piece',
           badge: {
             variant: 'danger',
-            text: 'NEW'
+            text: 'New'
           }
         },
         {
